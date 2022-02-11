cdc471418c20172a3430b1222012340b1c8b2a93
navjack <jack.mangano@gmail.com>
2022-02-11 17:47:42 -0500

    Update dbcompressor.py

--- dbcompressor.py
+++ dbcompressor.py
@@ -6,25 +6,30 @@ import zlib
 import json
 
 # Games databases.
-GAMEGEARDB      = "/Volumes/ext/games-picker/game gear.txt"
-GAMEBOYDB       = "/Volumes/ext/games-picker/gameboy.txt"
-NESDB           = "/Volumes/ext/games-picker/nes.txt"
-PS1DB           = "/Volumes/ext/games-picker/ps1.txt"
-SNESDB          = "/Volumes/ext/games-picker/snes.txt"
-N64DB           = "/Volumes/ext/games-picker/n64.txt"
-GBADB           = "/Volumes/ext/games-picker/gba.txt"
-GENDB           = "/Volumes/ext/games-picker/gen.txt"
-SMSDB           = "/Volumes/ext/games-picker/sms.txt"
-VBDB            = "/Volumes/ext/games-picker/vb.txt"
-SM3DB           = "/Volumes/ext/games-picker/sm3.txt"
-WSDB            = "/Volumes/ext/games-picker/ws.txt"
-S32XDB          = "/Volumes/ext/games-picker/32x.txt"
-TG16DB          = "/Volumes/ext/games-picker/tg16.txt"
-TCDDB           = "/Volumes/ext/games-picker/tcd.txt"
-NGPDB           = "/Volumes/ext/games-picker/neogeopocket.txt"
-FDSDB           = "/Volumes/ext/games-picker/fds.txt"
+GAMEGEARDB      = "game gear.txt"
+GAMEBOYDB       = "gameboy.txt"
+NESDB           = "nes.txt"
+PS1DB           = "ps1.txt"
+SNESDB          = "snes.txt"
+N64DB           = "n64.txt"
+GBADB           = "gba.txt"
+GENDB           = "gen.txt"
+SMSDB           = "sms.txt"
+VBDB            = "vb.txt"
+SM3DB           = "sm3.txt"
+WSDB            = "ws.txt"
+S32XDB          = "32x.txt"
+TG16DB          = "tg16.txt"
+TCDDB           = "tcd.txt"
+NGPDB           = "neogeopocket.txt"
+FDSDB           = "fds.txt"
 
-# open GAMEGEARDB and read it into a list and convert that list into JSON and then convert the JSON into a bytes-like object and then use zlib.compress() to compress the bytes-like object and then decompress it using zlib.decompress() and convert the bytes-like object into JSON and then convert that into a python list and display the list.
+# Open GAMEGEARDB and read it into a list and convert that list into
+# JSON and then convert the JSON into a bytes-like object and then
+# use zlib.compress() to compress the bytes-like object and then
+# decompress it using zlib.decompress() and convert the bytes-like
+# object into JSON and then convert that into a python list and
+# display the list.
 with open(GAMEGEARDB, "r") as GG:
     GG_LIST = GG.readlines()
     GG_LIST = [x.strip() for x in GG_LIST]
@@ -35,11 +40,16 @@ with open(GAMEGEARDB, "r") as GG:
     GG_DECOMPRESSED_JSON = GG_DECOMPRESSED.decode("utf-8")
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 
-# save GG_COMPRESSED as GAMEGEARDB + ".zlib"
+# Save GG_COMPRESSED as GAMEGEARDB + ".zlib"
 with open(GAMEGEARDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
 
-# do the same with the next database file
+# Save the decompressed data to verify.
+#with open(NESDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
+
+# Do the same with the next database file
 with open(GAMEBOYDB, "r") as GG:
     GG_LIST = GG.readlines()
     GG_LIST = [x.strip() for x in GG_LIST]
@@ -51,6 +61,9 @@ with open(GAMEBOYDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(GAMEBOYDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(GAMEBOYDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(NESDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -78,6 +91,9 @@ with open(PS1DB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(PS1DB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(PS1DB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(SNESDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -90,6 +106,9 @@ with open(SNESDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(SNESDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(SNESDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(N64DB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -102,6 +121,9 @@ with open(N64DB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(N64DB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(N64DB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(GBADB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -114,6 +136,9 @@ with open(GBADB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(GBADB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(GBADB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(GENDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -126,6 +151,9 @@ with open(GENDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(GENDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(GENDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(SMSDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -138,6 +166,9 @@ with open(SMSDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(SMSDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(SMSDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(VBDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -150,6 +181,9 @@ with open(VBDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(VBDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(VBDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(SM3DB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -162,6 +196,9 @@ with open(SM3DB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(SM3DB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(SM3DB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(WSDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -174,6 +211,9 @@ with open(WSDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(WSDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(WSDB) + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(S32XDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -186,6 +226,9 @@ with open(S32XDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(S32XDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(S32XDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(TG16DB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -198,6 +241,9 @@ with open(TG16DB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(TG16DB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(TG16DB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(TCDDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -210,6 +256,9 @@ with open(TCDDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(TCDDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(TCDDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(NGPDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -222,6 +271,9 @@ with open(NGPDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(NGPDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(NGPDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
 
 with open(FDSDB, "r") as GG:
     GG_LIST = GG.readlines()
@@ -234,3 +286,6 @@ with open(FDSDB, "r") as GG:
     GG_LIST = json.loads(GG_DECOMPRESSED_JSON)
 with open(FDSDB + ".zlib", "wb") as GG_ZLIB:
     GG_ZLIB.write(GG_COMPRESSED)
+#with open(FDSDB + ".list.txt", "wt") as GG_ZLIB:
+#    for item in GG_LIST:
+#        GG_ZLIB.write(str(item) + "\n")
